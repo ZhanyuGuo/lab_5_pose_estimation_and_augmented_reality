df6d6c19f779ab3953aa48d71c3d3785a2858f08
Zhanyu Guo <942568052@qq.com>
2021-06-18 17:44:38 +0800

    try push

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/18 17:43
+# @Author  : Zhanyu Guo
+# @Email   : 942568052@qq.com
+# @File    : helloworld.py
+# @Software: PyCharm
+if __name__ == '__main__':
+    print("hello world")
