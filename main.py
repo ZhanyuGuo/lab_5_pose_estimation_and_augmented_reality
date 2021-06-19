644a16f0f7d70296f1cee2949c1f7704876113a4
Zhanyu Guo <942568052@qq.com>
2021-06-19 14:38:07 +0800

    construct main.

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ from learning_vtk.viz3d import *
 
 
 def fake_main():
+    src_image = read_image('./images/world_A4.png')
+    dst_image = read_image('./images/camera_01.jpg')
+    kp1, desc1 = find_features(src_image)
+    kp2, desc2 = find_features(dst_image)
+
+    # #  brute_force_match(BFM)
+    # src_pts, dst_pts, good = brute_force_match(kp1, kp2, desc1, desc2)
+    # h_matrix, mask = cal_homography(src_pts, dst_pts)
+
+    #  flann_match(knn)
+    src_pts, dst_pts, good = flann_match(kp1, kp2, desc1, desc2)
+    h_matrix, mask = cal_homography(src_pts, dst_pts)
+    print(h_matrix)
+
+    draw_plots(h_matrix, mask, src_image, dst_image, kp1, kp2, good)
     pass
 
 
